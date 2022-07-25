cf0a3e59e63a165a90dd8dc67a32fa59793bb4fb
Pingusama <109974527+Pingusama@users.noreply.github.com>
2022-07-26 00:24:29 +0800

    Create LAB9_ISR2.C

---
+++ LAB9_ISR2.C
@@ -0,0 +1,16 @@
+#include <xc.h>
+#include "config.h"
+
+
+void __interrupt() isr(void)
+{
+    if(PIR0bits.TMR0IF == 1)
+    {
+        PIR0bits.TMR0IF = 0;
+        PORTAbits.RA0 = ~PORTAbits.RA0;
+        PORTAbits.RA1 = ~PORTAbits.RA1;
+        
+        TMR0H = 0x0B;
+        TMR0L = 0xDC;
+    }
+}
